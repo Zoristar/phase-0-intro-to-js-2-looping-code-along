7e1939a64a4e990e7d75d84c604849cedecf14a8
Zoriana <zoriana.shatkovska@gmail.com>
2022-11-04 21:28:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, event){
+    const newArray = []
+    for(let i = 0; i < namesArray.length; i++){
+        newArray[i] = `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` 
+    }
+    return newArray
+}
+
+function countDown(integer){
+    let i = 0
+    while ( i <= integer) {
+        console.log(i);
+        i++;
+      }
+
+}
+  
